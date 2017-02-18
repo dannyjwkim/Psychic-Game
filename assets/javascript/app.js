1bd85a92cefa699c40d7b2c9ba477ced548a92a6
Daniel Kim <dannyjwkim@Daniels-MacBook-Pro.local>
2017-02-18 05:32:17 -0500

    cleaned up code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,5 @@
-//Letter choices available
-var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var computerOptions = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-//Setting all to zero
 var wins = 0;
 var losses = 0;
 var guesses = 9;
@@ -9,26 +7,19 @@ var guessesLeft = 9;
 var guessedLetters = [];
 var letterToGuess = null;
 
+var computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
 
-
-//Lets the computer select a random letter from the available choices
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-//Allows the user 9 guesses
-// guesses = guesses || 9
 var updateGuessesLeft = function() {
-  // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
-  document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
+  document.querySelector("#guessLeft").innerHTML = "Guesses left: " + guessesLeft;
 };
 
 var updateLetterToGuess = function() {
-  this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+  this.letterToGuess = this.computerOptions[Math.floor(Math.random() * this.computerOptions.length)];
 };
 var updateGuessesSoFar = function() {
-  // Here we take the guesses the user has tried -- then display it as letters separated by commas. 
-  document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
+  document.querySelector("#letter").innerHTML = "Your Guesses so far: " + guessedLetters.join(", ");
 };
-// Function will be called when we reset everything
+
 var reset = function() {
   totalGuesses = 9;
   guessesLeft = 9;
@@ -43,28 +34,28 @@ updateLetterToGuess();
 updateGuessesLeft();
 
 
-//When key is released it becomes the users guess
 document.onkeyup = function(event) {
-    guessesLeft--;
+  guessesLeft--;
   var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
   guessedLetters.push(userGuess);
   updateGuessesLeft();
   updateGuessesSoFar();
 
-        if (guessesLeft > 0){
-            if (userGuess == letterToGuess){
-                wins++;
-                document.querySelector('#wins').innerHTML = "Wins: " + wins;
-                alert("Whaaaaat, you ARE psychic!");
-                reset();
-            }
-        }else if(guessesLeft == 0){
-            // Then we will loss and we'll update the html to display the loss 
-            losses++;
-            document.querySelector('#losses').innerHTML = "Losses: " + losses;
-            alert("Surprise, surprise, you're not psychic. Try again!");
-            // Then we'll call the reset. 
-            reset();
-        }
+  if (guessesLeft > 0) {
+    if (userGuess == letterToGuess){
+      wins++;
+      document.querySelector("#wins").innerHTML = "Wins: " + wins;
+      alert("Whaaaaat, you ARE psychic!");
+      reset();
+    }
+  }     
+        
+    else if(guessesLeft == 0) {
+      losses++;
+      document.querySelector("#losses").innerHTML = "Losses: " + losses;
+      alert("Surprise, surprise, you're not psychic. Try again!");
+      reset();
+    }
+  
 };
